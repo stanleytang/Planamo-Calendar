784b284a2a030db168c69bc882290411884afa4a
Andy Fang <andyfang@stanford.edu>
2012-03-10 15:00:25 -0800

    removed polls from settings.py

--- planamo/settings.py
+++ planamo/settings.py
@@ -119,7 +119,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-		'polls',
 		'planamocal',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
