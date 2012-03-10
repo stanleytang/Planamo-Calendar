749e84c3dc39e6efa4bb15b8c58aa4d34787e714
Stanley Tang <account@stanleytang.com>
2012-03-10 15:12:33 -0800

    Basic ajax working

--- planamo/planamocal/urls.py
+++ planamo/planamocal/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('planamocal.views',
 		url(r'^$', 'index'),
+		url(r'^createEvent/$', 'createEvent'),
 )
--- planamo/planamocal/views.py
+++ planamo/planamocal/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.shortcuts import render_to_response, get_object_or_404
 from planamocal.models import Calendar
+from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
 	calendar = get_object_or_404(Calendar, pk=1)
@@ -8,3 +9,16 @@ def index(request):
 		'planamocal/index.html',
 		{'calendar': calendar},
 	)
+	
+from django.views.decorators.csrf import csrf_exempt #temp solution
+@csrf_exempt #temp solution
+def createEvent(request):
+	if request.is_ajax():
+		if request.method == 'GET':
+			message = "This is an XHR GET request"
+	  	elif request.method == 'POST':
+	  		message = "This is an XHR POST request"
+	    	# Here we can access the POST data
+	else:
+		message = "No XHR"
+	return HttpResponse(message)
